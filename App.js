2fd1d254232ec5bdd7cf14a6bea59bdd77c83c5c
MatheusAraujo02 <127919173+MatheusAraujo02@users.noreply.github.com>
2024-08-07 20:40:34 -0300

    Create a modal

--- App.js
+++ App.js
@@ -1,39 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { useState} from 'react';
 
 import Button from './src/button';
 import ImageViewer from './src/ImageViewer';
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
+  const [showAppOptions, setShowAppOptions] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri)
+      setShowAppOptions(true);
+    } else {
+      alert('Você não escolheu nenhuma imagem.')
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer 
+        placeholderImageSource={PlaceholderImage} 
+        selectedImage={selectedImage}
+        />
       </View> 
+    {showAppOptions ?(
+      <View />
+    ) : (
       <View style={styles.footerContainer}>
-        <Button label="Escolha uma foto"/>
-        <Button label="Use esta foto"/>  
-      </View>    
+        <Button theme="primary" label="Escolha uma foto" onPress={ pickImageAsync	}/>
+        <Button label="Use esta foto" onPress={() => setShowAppOptions(true)}/>  
+      </View>
+      )}    
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   imageContainer: {
     flex: 1,
     paddingTop: 58,
   },
   footerContainer: {
-    fkex: 1 / 3,
+    flex: 1 / 3,
     alignItems: 'center',
   },
  
